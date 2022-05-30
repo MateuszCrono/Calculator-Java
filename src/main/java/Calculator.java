441742af28fc33411adcfbdbb142b0c81f2716c4
Mateusz Olszewski <matcrono@gmail.com>
2022-05-31 00:37:59 +0800

    Formatted code

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,15 +1,16 @@
 public class Calculator {
 
     public static int addingMethod(int a, int b) {
-        return a+b;
+        return a + b;
     }
-        public static int deductingtMethod(int a, int b) {
-            return a-b;
-        }
 
-    public static void main (String[] args) {
+    public static int deductingtMethod(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        System.out.println(addingMethod(2,5));
-        System.out.println(deductingtMethod(5,2));
+        System.out.println(addingMethod(2, 5));
+        System.out.println(deductingtMethod(5, 2));
     }
 }
